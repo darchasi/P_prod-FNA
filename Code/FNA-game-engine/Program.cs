1116a46d2a2abd8611be50218f15aed917ca6755
Nima Zarrabi <pu78ifh@eduvaud.ch>
2024-09-19 10:57:15 +0200

    FNA base code fonctional

---
+++ Code/FNA-game-engine/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNA_game_engine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+
+}
