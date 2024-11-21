a8da7b71b5befb529dd10b56f66de77bacfd8284
darchasi <pv20qck@eduvaud.ch>
2024-11-21 11:26:32 +0100

    file firecharacter, in develp error stackoverflow

--- Code/FNA-game-engine/FireCharacter.cs
+++ Code/FNA-game-engine/FireCharacter.cs
@@ -24,50 +24,60 @@ namespace FNA_game_engine
         {
             if (projectiles.Count == 0)
             {
-                for (int i = 0; i < numOfProjectiles; i++)
+                /*for (int i = 0; i < numOfProjectiles; i++)
                 {
                     projectiles.Add(new Projectile());
-                }
+                }*/
+                projectiles.AddRange(Enumerable.Range(0, numOfProjectiles).Select(_ => new Projectile()));
             }
             base.Initialize();
         }
 
         public void Fire()
         {
-            for (int i =0; i < numOfProjectiles;i++)
+            /*for (int i =0; i < numOfProjectiles;i++)
             {
                 if (!projectiles[i].active)
                 {
                     projectiles[i].Fire(this, position, direction);
                     break;
                 }
-            }
+            }*/
+
+            projectiles.ToList().FirstOrDefault(p => !p.active)?.Fire(this, position, direction);
         }
 
         public override void Load(ContentManager content)
         {
-            for (int i = 0; i < numOfProjectiles; i++)
+            /*for (int i = 0; i < numOfProjectiles; i++)
             {
                 projectiles[i].Load(content);
-            }
+            }*/
+
+            projectiles.ToList().ForEach(p => p.Load(content));
             base.Load(content);
         }
 
         public override void Update(List<GameObject> objects, Map map)
         {
-            for (int i = 0; i < numOfProjectiles; i++)
+            /*for (int i = 0; i < numOfProjectiles; i++)
             {
                 projectiles[i].Update(objects, map);
-            }
+            }*/
+
+            projectiles.ToList().ForEach(projectile => Update(objects, map));
             base.Update(objects, map);
         }
 
         public override void Draw (SpriteBatch spriteBatch)
         {
+            /*
             for (int i = 0; i < numOfProjectiles; i++)
             {
                 projectiles[i].Draw(spriteBatch);
-            }
+            }*/
+
+            projectiles.ToList().ForEach(p => p.Draw(spriteBatch));
             base.Draw(spriteBatch);
         }
     }
