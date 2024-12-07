8b927a42fe9ed0a10ad4090740a88db0b1915d1e
NimaETML <pu78ifh@eduvaud.ch>
2024-12-07 11:06:02 +0100

    applygravity arrow

--- Code/FNA-game-engine/Projectile.cs
+++ Code/FNA-game-engine/Projectile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -6,9 +7,10 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace FNA_game_engine
 {
-    public class Projectile : GameObject
+    public class Projectile : GravityPbject
     {
-        const float speed = 12f;
+        const float PROJDECEL = 1;
+        const int baseXvelocity = 2;
         Character owner;
         int destroyTimer;
         const int maxTimer = 180; // bullet destroy timer in ticks (60ticks/sec)
@@ -16,6 +18,7 @@ namespace FNA_game_engine
         public Projectile()
         {
             active = false;
+            playerCollidable = false;
         }
 
         public override void Load(ContentManager content)
@@ -31,11 +34,7 @@ namespace FNA_game_engine
                 return;
             }
 
-            // Update movement
-            position += direction * speed;
-
-            CheckCollision(objects, map);
-            
+            UpdateMovement(objects, map);
             // Update deathtimer
             destroyTimer--;
             if (destroyTimer <= 0 && active)
@@ -52,6 +51,15 @@ namespace FNA_game_engine
             owner = inputOwner;
             position = inputPositon;
             direction = inputDirection;
+            decel = PROJDECEL;
+            if (direction.X > 0)
+            {
+                velocity.X = baseXvelocity;
+            }
+            else
+            {
+                velocity.X = -baseXvelocity;
+            }
             active = true;
             destroyTimer = maxTimer;
         }
@@ -64,23 +72,130 @@ namespace FNA_game_engine
             spriteBatch.Draw(image,position,null,Color.White,0f, Vector2.Zero,1f,spriteEffect,0f);
         }
 
-        private void CheckCollision(List<GameObject> objects, Map map)
+        public override void UpdateMovement(List<GameObject> objects, Map map)
         {
-
-            var collidedObject =  objects.FirstOrDefault(obj => obj.active && obj != owner && obj.projectileCollidable && obj.CheckCollision(boundingBox));
-
-            if (collidedObject != null)
+            // Appliquer la gravité avant de vérifier les collisions
+            if (applyGravity)
             {
-                Destroy();
-                collidedObject.ProjectileResponse();
-                return;
+                ApplyGravity(map);
             }
 
-            //Verify collision
-            if (map.CheckCollision(boundingBox) != Rectangle.Empty)
+            // Gérer les mouvements horizontaux
+            if (velocity.X != 0)
             {
-                Destroy();
+                // Vérifier la collision horizontale avant de déplacer
+                if (!CheckCollisions(map, objects, true)) // true = vérifier les collisions horizontales
+                {
+                    position.X += velocity.X; // Appliquer le mouvement si pas de collision
+                }
+                else
+                {
+                    Destroy();
+                }
             }
+
+            // Gérer les mouvements verticaux
+            if (velocity.Y != 0)
+            {
+                // Vérifier la collision verticale avant de déplacer
+                if (!CheckCollisions(map, objects, false)) // false = vérifier les collisions verticales
+                {
+                    position.Y += velocity.Y; // Appliquer le mouvement si pas de collision
+                }
+                else
+                {
+                    Destroy();
+                }
+            }
+
+            // Décélération pour les mouvements horizontaux
+            //velocity.X = TendToZero(velocity.X, decel);
+
+            // Décélération verticale uniquement si la gravité est désactivée
+            if (!applyGravity)
+            {
+                velocity.Y = TendToZero(velocity.Y, decel);
+            }
+        }
+
+        public override bool CheckCollisions(Map map, List<GameObject> objects, bool xAxis)
+        {
+            Rectangle futureBoundingBox = boundingBox;
+
+            int maxX = (int)(Math.Abs(velocity.X) + accel);
+            int maxY = (int)(velocity.Y + gravity);
+            /*
+            if (applyGravity)
+            {
+                maxY = (int)jumpVelocity;
+            }*/
+
+            // Stop movement at X contact
+            if (xAxis && velocity.X != 0)
+            {
+                if (velocity.X > 0)
+                {
+                    futureBoundingBox.X += maxX;
+                }
+                else
+                {
+                    futureBoundingBox.X -= maxX;
+                }
+            }
+            // Stop movement at Y contact
+            else if (!applyGravity && !xAxis && velocity.Y != 0)
+            {
+                if (velocity.Y > 0)
+                {
+                    futureBoundingBox.Y += maxY;
+                }
+                else
+                {
+                    futureBoundingBox.Y -= maxY;
+                }
+            }
+            // Stop movement at Y contact
+            else if (applyGravity && !xAxis && velocity.Y != gravity)
+            {
+                if (velocity.Y > 0)
+                {
+                    futureBoundingBox.Y += maxY;
+                }
+                else
+                {
+                    futureBoundingBox.Y -= 20;
+                }
+            }
+
+            // Check for wall collision
+            Rectangle wallCollision = map.CheckCollision(futureBoundingBox);
+
+            if (wallCollision != Rectangle.Empty)
+            {
+                // Lands character on floor if it gets too close
+                if (applyGravity && velocity.Y >= gravity && (futureBoundingBox.Bottom > wallCollision.Top) && (boundingBox.Right > wallCollision.Left) && (boundingBox.Left < wallCollision.Right))
+                {
+                    LandResponse(wallCollision);
+                    return true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            // Check for object collisions
+
+            for (int i = 0; i < objects.Count; i++)
+            {
+                if (objects[i] != this && objects[i].active && objects[i] != owner && objects[i].projectileCollidable && objects[i].CheckCollision(futureBoundingBox))
+                {
+                    return true;
+                }
+            }
+
+            // If no collision detected
+            return false;
         }
 
         public void Destroy()
